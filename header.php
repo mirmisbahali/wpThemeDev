bd479ec19f570c11b5340d8ea4d82fc0ec5a9a59
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-04 17:51:28 +0530

    fixed language_attributes() function

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 */
 ?>
 <!DOCTYPE html>
-<html lang="<?php language_attibutes(); ?>">
+<html lang="<?php language_attributes(); ?>">
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
