93f21f76a988a120f7fa09911dc0bd9c6b8abc4f
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-04 18:06:14 +0530

    modified functions.php to include ./assets/main.js in the footer

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@
 function aquila_enqueue_scripts() {
 
     wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+    wp_enqueue_script('main.js', get_template_directory_uri() . '/assets/main.js', filemtime( get_template_directory() . '/style.css' ), true );
 
 }
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
