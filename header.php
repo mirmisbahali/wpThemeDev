932a2e374a7f0741df67e4c4c69e290553235703
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-04 18:26:13 +0530

    added bootstrap

--- header.php
+++ header.php
@@ -13,5 +13,9 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+<?php 
+    if (function_exists('wp_body_open')) {
+        wp_body_open();
+    }
+?>
 <header>Header</header>
