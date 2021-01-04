5239b8e2f229b9905e3d557660479e20aed1c351
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-04 17:50:12 +0530

    added dynamic charset, wp_head and wp_footer functions

--- header.php
+++ header.php
@@ -5,11 +5,12 @@
 */
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attibutes(); ?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wordpress Theme</title>
+    <?php wp_head(); ?>
 </head>
 <body>
 <header>Header</header>
